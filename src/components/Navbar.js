75150fed8046e15f92990d94f0685d827ababbc8
Jose Mendes <ja.gaeta@hotmail.com>
2020-02-12 09:17:31 -0300

    Reinstalada reactstrap

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap"
 
-const Navbar = () => {
-  return <div>Esta é a Navbar</div>
+const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggle = () => setIsOpen(!isOpen)
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="ml-auto" navbar>
+            <NavItem>
+              <NavLink href="/components/">Components</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="https://github.com/reactstrap/reactstrap">
+                GitHub
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  )
 }
 
-export default Navbar
+export default NavBar
